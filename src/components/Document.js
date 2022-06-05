0eab2f188571c7346a645b48b62e10bc2c3eb6b6
Vishal Pandey <vishalpandey917@gmail.com>
2022-06-05 16:21:53 +0530

    Merge branch 'master' of https://github.com/rahulp-github/codedamn-hackfest

--- src/components/Document.js
+++ src/components/Document.js
@@ -1,51 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 export default function Document() {
+    const [paragraphCount, setParagraphCount] = useState(1);
+    const [documentText, setDocumentText] = useState({title: ""});
+    const commands = [
+        {
+          command: 'Alexa change title *',
+          callback: (speech) => {
+            console.log(speech)
+            setDocumentText({...documentText, title: speech})
+          }
+        },
+        {
+          command: 'Alexa new paragraph *',
+          callback: (speech) => {
+            console.log(speech)
+            setDocumentText({...documentText, paragraph: speech})
+            console.log(documentText)
+            setParagraphCount(paragraphCount + 1)
+          }
+        }
+      ]
+    
+      const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+      } = useSpeechRecognition({commands});
+    
+      if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+      }
     return (
-        <div className='document-container'>
-            <div className='wrapper'>
-                <div className='top-buttons'>
-                    <div className='back-button'>
-                        <Link to="/" ><i class="icon fa-solid fa-arrow-left fa-2x"></i></Link>  
+        <div>
+            <div className='document-container'>
+                <div className='wrapper'>
+                    <div className='top-buttons'>
+                        <div className='back-button'>
+                            <Link to="/" ><i className="icon fa-solid fa-arrow-left fa-2x"></i></Link>
+                        </div>
+                        <div className='save-button'>
+                            <i className="icon fa-solid fa-floppy-disk fa-2x"></i>
+                        </div>
+                        <div className='download-button'>
+                            <i className="icon fa-solid fa-download fa-2x"></i>
+                        </div>
+                        <div className='share-button'>
+                            <i className="icon fa-solid fa-share-nodes fa-2x"></i>
+                        </div>
                     </div>
-                    <div className='save-button'>
-                        <i class="icon fa-solid fa-floppy-disk fa-2x"></i>
-                    </div>
-                    <div className='download-button'>
-                        <i class="icon fa-solid fa-download fa-2x"></i>
-                    </div>
-                    <div className='share-button'>
-                        <i class="icon fa-solid fa-share-nodes fa-2x"></i>
+                    <div className='document-area'>
+                        <div className='document-title'>
+                            <h1>{documentText.title!="" ? documentText.title : "Title not confirmed!"}</h1>
+                        </div>
+                        <div className='document-body'>
+                            <p>{documentText.paragraph!="" ? documentText.paragraph : "paragraph not confirmed!"}</p>
+                            {transcript}
+                        </div>
                     </div>
                 </div>
-                <div className='document-area'>
-                    <div className='document-title'>
-                        <h1>Document Title</h1>
+            </div>
+            <div className="mic-transcript-container">
+                <div className="wrap-mic-transcript">
+                    <div className="transcript-area"> {listening}
                     </div>
-                    <div className='document-body'>
-                        lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type
-                        specimen book. It has survived not only five centuries, but also the leap int
-                        o electronic typesetting, remaining essentially unchanged. It was popularised
-                        in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                        and more recently with desktop
-                        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                        Why do we use it?
-                        It is a long established fact that a reader will be distracted by the readable
-                        content of a page when looking at its layout. The point of using Lorem Ipsum
-                        is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
-                        content here', making it look like readable English. Many desktop publishing packages and web page
-                        editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover
-                        many web sites still in their infancy. Various versions have evolved over the years, sometimes by
-                        accident, sometimes on purpose (injected humour and the like).
-                        Where does it come from?
-                        Contrary to popular belief, Lorem Ipsum is not simply random text.
-                        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more
-                        obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word
-                        in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
+                    <div className='mic-area'>
+                        <span className='mic'>
+                            <i className="mic-icon fa-solid fa-microphone fa-2x" onClick={()=>SpeechRecognition.startListening({
+                                continuous: true,
+                                language: 'en-US'
+                            })}></i>
+                        </span>
                     </div>
                 </div>
             </div>
